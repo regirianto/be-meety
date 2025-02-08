59d76e5a9bf5e4570f39adc078d49b3de1d1aab9
regirianto <regirianto22@gmail.com>
2025-02-08 14:01:45 +0700

    testing deploy 5

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // Routes
