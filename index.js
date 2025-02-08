3a48f3827e17927a54d53ed1b250f925fa8c05c3
regirianto <regirianto22@gmail.com>
2025-02-08 13:57:41 +0700

    testing deploy 4

--- index.js
+++ index.js
@@ -27,4 +27,5 @@ app.get("/api/likes", likesRoutes);
 app.get("/api/chat", chatRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
